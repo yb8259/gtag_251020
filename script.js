2e9970d1f9a5dafdd89ba330ddcaee3e0cf768c6
“yb8259” <yb8259@naver.com>
2025-10-21 11:08:27 +0900

    4

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 검색 이벤트 함수수
 const handleSearch = () => {
   const input = document.getElementById("searchInput").value.trim();
 
@@ -12,4 +13,66 @@ const handleSearch = () => {
   } else {
     alert("검색어를 입력하세요");
   }
+};
+// 상품상세보기 이벤트 함수
+const viewItem = () => {
+  gtag("event", "view_item", {
+    category: "IT",
+    labels: "promotion_item",
+    currency: "KRW",
+    value: 129000,
+    items: [
+      {
+        item_id: "EP1001",
+        item_name: "무선 이어폰",
+        currency: "KRW",
+        price: 129000,
+        quantity: 1,
+      },
+    ],
+  });
+  alert("view_item 이벤트 발생!");
+};
+// 장바구니 이벤트 함수
+const addToCart = () => {
+  gtag("event", "add_to_cart", {
+    category: "IT", // category, labelss 비필수 값
+    labels: "promotion_item",
+    currency: "KRW", // currency, value, items 필수값
+    value: 129000,
+    items: [
+      {
+        item_id: "EP1001",
+        item_name: "무선 이어폰",
+        currency: "KRW",
+        price: 129000,
+        quantity: 1,
+      },
+    ],
+  });
+  alert("add_to_cart 이벤트 발생!");
+};
+
+// 상품구매하기 이벤트 함수
+const purchase = () => {
+  gtag("event", "purchase", {
+    transaction_id: "T1234",
+    affiliation: "온라인몰",
+    category: "IT",
+    currency: "KRW",
+    value: 129000,
+    labels: "promotion_item",
+    tax: 12900,
+    shipping: 3000,
+    items: [
+      {
+        item_id: "EP1001",
+        item_name: "무선 이어폰",
+        currency: "KRW",
+        price: 129000,
+        quantity: 1,
+      },
+    ],
+  });
+  alert("purchase 이벤트 발생!");
 };
